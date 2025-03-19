3418604f05146928e1b6aa81573f6ce5c87459da
Milo S <104745208+MiloStone@users.noreply.github.com>
2025-03-19 19:04:24 -0400

    Fixed Camera Movement Scripts
    Created global for all camera speed needs to refer to, deleted useless move objects

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;  // The player capsule
-    public Vector3 offset = new Vector3(0, 5, -10); // Camera's position offset
-    public Vector3 lookOffset = new Vector3(0, 0, 0);
+    public static float cameraSpeed = 15f;
+
     void LateUpdate()
     {
-        // Follow the target
-        transform.position = target.position + offset;
-        
-        transform.LookAt(target.position + lookOffset); // Keep the camera focused on the player
+        Vector3 cameraVector = new Vector3(0, 0, cameraSpeed * Time.deltaTime);
+        transform.position += cameraVector;
     }
 }
+
--- Assets/Scripts/SphereRolling.cs
+++ Assets/Scripts/SphereRolling.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class SphereRolling : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Speed at which the sphere moves
     public float rotationSpeed = 360f; // Rotation speed for rolling (degrees per second)
 
     void Update()
     {
         // Move the sphere forward at the specified speed
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * CameraFollow.cameraSpeed * Time.deltaTime, Space.World);
 
         // Rotate the sphere to simulate rolling
-        float rotationAmount = moveSpeed * Time.deltaTime * (360f / (2 * Mathf.PI * transform.localScale.x));
+        float rotationAmount = CameraFollow.cameraSpeed * Time.deltaTime * (360f / (2 * Mathf.PI * transform.localScale.x));
         transform.Rotate(rotationAmount, 0, 0, Space.Self);
     }
 }
